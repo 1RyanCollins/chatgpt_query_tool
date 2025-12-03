1319e7d6644b72aed750501ee123133e7b0dae50
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2025-12-02 22:26:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import streamlit as st
 import re
 import time
 
